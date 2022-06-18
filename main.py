8e4fd800c0cf3fe8521b874b6f0a6884345c6e92
Gabriel Braden <gabriel.braden@cision.com>
2022-06-18 10:18:05 -0400

    Added main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+"""Main module of printer bot. This file entrypoint of the program.
+"""
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+# --------------------- Objects ------------------------------
+
+# --------------------- Helper Functions ---------------------
+
+# --------------------- Execution ----------------------------
